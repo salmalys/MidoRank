49496d5d4f4f1ae4593a3dbd00d0203a696d6a59
picardalix <98279717+picardalix@users.noreply.github.com>
2022-01-28 23:39:36 +0100

    Update list.c

--- list.c
+++ list.c
@@ -6,7 +6,7 @@
 
 List* list_init(char* name){
     List *l = malloc(sizeof(List));
-    if (l == NULL) return NULL;
+    if (l == NULL) return NULL;                                             //Test si la liste l n'est pas nulle : obligatoire pour pouvoir appliqué les lignes suivantes à la liste l
     l->name = name;
     l->first = NULL;
     l->cpt_sort = 1;
@@ -15,16 +15,16 @@ List* list_init(char* name){
 
 Node* node_init(char* p){
     Node* s = malloc(sizeof(Node));
-    if (s == NULL) return s;
+    if (s == NULL) return s;                                                //Test obligatoire
     s->name = p;
     s->next = NULL;
     return s;
 }
 
 void add_list(List* l, char* p){
-    if (l == NULL) return;
+    if (l == NULL) return;                                                  //Test obligatoire
     Node* s = node_init(p);
-    if (s == NULL) return;
+    if (s == NULL) return;                                                  //Vérifie si la fonction node_init a bien initialisé le noeud s
     s->next = l->first;
     l->first = s;
 }
@@ -56,19 +56,18 @@ void free_list(List* l){
     Node* s;
     while(n!=NULL){
         s=n;
-        n=n->next;
-        s->name=NULL;
-        free(s->name);
-        free(s);
+        n=n->next;                                                              //Permet de parcourir toutes les liste l
+        s->name=NULL;                                                           //Met à nul le nom de chaque noeud
+        free(s->name);                                                          //Libère l'espace pour le nom de chaque noeud
+        free(s);                                                                //Libère le noeud
     }
-    l=NULL;
-    free(l);
+    l=NULL;                                                                     //Met à nul la liste
+    free(l);                                                                    //Libèere la liste
 }
 
 
-void free_graph(List Adj[], int n){
+void free_graph(List Adj[], int n){                                             //Libère Adj
     for (int i=0; i<n+1; i++){
-      //printf("%s\n", Adj[i].name);
-      free_list(&Adj[i]);
+      free_list(&Adj[i]);                                                       //Libère chaque liste comprise dans Adj
     }
 }
