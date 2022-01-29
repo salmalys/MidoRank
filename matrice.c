ac773f654f6a3bf9d616942d54d3f4e2a691e426
salmalys <98278422+salmalys@users.noreply.github.com>
2022-01-29 14:40:24 +0100

    Create matrice.c

--- matrice.c
+++ matrice.c
@@ -5,14 +5,14 @@
 #include "list.h"
 #include "calcul.h"
 #include "matrice.h"
-#define SIZE_BUFFER_line 100000                                                                        //Defini une constante pour la taille des lignes
-#define SIZE_BUFFER_name 1000                                                                          //Defini une constante pour la taille des caractères
+#define SIZE_BUFFER_line 100000                                                                        //constante pour la taille des lignes
+#define SIZE_BUFFER_name 1000                                                                          //constante pour la taille des noms de pages
 
 int size_matrix(FILE* f){
   int n = 0;
   char line[SIZE_BUFFER_line];
     while(fgets(line, sizeof(line), f)){ 
-    if (strcmp(line,"\n")==0){                                                                         //Permet de s'arreter à la premiere ligne qui est égal à un retour à la ligne, pour ne pas compter les lignes vace les Vecteurs résultat
+    if (strcmp(line,"\n")==0){                                                                         //s'arrete à la premiere ligne qui est égal à un retour à la ligne, pour ne pas compter la ligne vide et la ligne avec le vecteur resultat
             return n; 
         }
     n++;                                                  
@@ -26,9 +26,9 @@ void graph_init_mat(FILE *f, List Adj[],List Adj_pred[], int n){                
     Adj_pred[0] = *(list_init("SuperNode"));                                                             //Initialise la liste de Adj_pred du SuperNoeud
     for (int w=1; w<=n+1; w++){                                                                                          
         char* value2 = malloc(sizeof(char*));                                                                                                 
-        sprintf(value2,"%d",w);                                                                          //Permet de mettre l'entier w dans value2 en le transformant en chaine de characteère
-        Adj_pred[w]=*(list_init(value2));                                                                //Initialise les noms de chaque liste de Adj_pred de la page 1 à celle n+1 
-        add_list(&Adj_pred[w],value2);                                                                   //Ajoute lui même à la liste de Adj_pred correspondantes, car chaque page est pointée par elle même
+        sprintf(value2,"%d",w);                                                                          //Permet de mettre l'entier w dans value2 en le transformant en chaine de caracteres
+        Adj_pred[w]=*(list_init(value2));                                                                //Initialise les noms de chaque liste de Adj_pred de l'indice 1 a n+1 
+        add_list(&Adj_pred[w],value2);                                                                   //Ajoute lui meme à la liste de Adj_pred correspondantes, car chaque page est pointee par elle même
         add_list(&Adj_pred[w],"SuperNode");                                                              //Ajoute la page du SuperNoeud à la liste Adj_pred
     }
     for (int i=1; i<=n; i++){                                                                            //Parcourt les lignes
@@ -39,7 +39,7 @@ void graph_init_mat(FILE *f, List Adj[],List Adj_pred[], int n){                
      for(int j=0; j<n; j++){                                                                              //Parcourt sur les colonnes
          int v=0;
          fscanf(f, "%d", &v);                                                                             //Parcourt le fichier et recupère le premier entier qu'il trouve et le stock dans v
-         if(v==1){                                                                                        //Si l'entier récuperer est un 1 :
+         if(v==1){                                                                                        //Si l'entier recuperé est un 1 :
             cpt++;                                                                                        //Incremente le compteur car il y a un voisin sortant en plus
             char* value1 = malloc(sizeof(char*));
             sprintf(value1,"%d",j+1);                                                                      //Met l'entier j+1 dans le charactère value1, j+1 car on commence à la colonne 0 mais cette colonne on considère que son nom est 1
@@ -47,41 +47,41 @@ void graph_init_mat(FILE *f, List Adj[],List Adj_pred[], int n){                
             add_list(&Adj_pred[j+1], value);                                                               //Ajoute au tableau des predecesseurs de la page du nom value 1, la page de nom value
     }
      }
-      cpt++;                                                                                               //Incremente le compteur une fois car lui même appartient à sa liste
+      cpt++;                                                                                               //Incremente le compteur une fois car lui meme appartient à sa liste
       Adj[i].cpt_sort = cpt;                                                                   
-      add_list(&Adj[i],value);                                                                              //S'ajoute lui même
+      add_list(&Adj[i],value);                                                                              //S'ajoute lui meme
       add_list(&Adj[i],"SuperNode");                                                                        //Ajoute la page du SuperNoeud 
       add_list(&Adj_pred[0],value);                                                                         //Ajoute chaque nom de liste au Adj_pred du SuperNoeud
     }
-     Adj[0].name="SuperNode";                                                                               //Reinitialisation de Adj[0]
+     Adj[0].name="SuperNode";                                                                               //initialisation de la liste du SuperNoeudAdj[0]
      Adj[0].first=NULL;                                                                       
      Adj[0].cpt_sort = n;
 
 }
 
 
-Page* recup_vec(FILE* f, int n){                                                                            //On se trouve à la ligne avant les Vecteurs Résultat
-  Page* Vec_Res = malloc(sizeof(Page)*(n+1));                                            
-  char line[256];
-  fgets(line, sizeof(line), f);                                                                             //Permet d'être à la ligne des vecteurs résultats                                                                                   
+Page* recup_vec(FILE* f, int n){                                                                            //renvoie d'un vecteur resultat contenant les pageRank du fichier                                                                         
+  Page* Vec_Res = malloc(sizeof(Page)*(n+1));                                                               
+  char line[256];                                                                                           //le fichier n'a pas ete ferme, lecture de la suite 
+  fgets(line, sizeof(line), f);                                                                             //permet d'etre à la ligne du vecteur résultat                                                                                   
 
-  for(int i = 0; i<n+1; i++){
-    if (i == 0) Vec_Res[i].name = "SuperNode";                                                              //Initialise le nom de Vec_Res[0] à SuperNodde
+  for(int i = 0; i<n+1; i++){                                                                                                                                       
+    if (i == 0) Vec_Res[i].name = "SuperNode";                                                              //initialise le nom de Vec_Res[0] à SuperNoeud
     else{
       char* value = malloc(SIZE_BUFFER_name);                                             
       sprintf(value,"%d",i);
-      Vec_Res[i].name = value;                                                                              //Initialise les noms des Vec_Res de 1 à n+1
+      Vec_Res[i].name = value;                                                                              //initialise les noms du Vec_Res de 1 à n+1
     }
-    fscanf(f,"%Lf, ", &Vec_Res[i].pageRank);                                                                //Permet de récuperer chaque nombre sous la forme d'un long double et le met dans le pageRank du Vec_Res 
+    fscanf(f,"%Lf, ", &Vec_Res[i].pageRank);                                                                //recupere chaque nombre sous la forme d'un long double et le met dans le pageRank du Vec_Res 
   }
   return Vec_Res;
 }
 
 
-void moy_max(Page Vec_Ex[], Page Vec_Res[], int n){                                                         //Calcul la moyenne et le maximum des écarts entre les Vecteurs Resultats et les Vecteurs Experimentaux
-    float ecart_max[n+1];                                                                                   //Créer tableau des écarts maximum
+void moy_max(Page Vec_Ex[], Page Vec_Res[], int n){                                                         //calcul et affichage de la moyenne et le maximum des ecarts entre le vecteur resultat et le vecteur experimental
+    float ecart_max[n+1];                                                                                   //creer tableau des ecarts maximum
     float moy=0;  
-    ecart_max[0]=(fabs((Vec_Ex[0].pageRank - Vec_Res[0].pageRank)/Vec_Res[0].pageRank))*100;                //fabs() : valeur absolue pour les double
+    ecart_max[0]=(fabs((Vec_Ex[0].pageRank - Vec_Res[0].pageRank)/Vec_Res[0].pageRank))*100;                //fabs() : valeur absolue pour double
     float max = ecart_max[0];                                                                               
     for (int i = 1; i<n+1; i++){
         ecart_max[i] = (fabs((Vec_Ex[i].pageRank - Vec_Res[i].pageRank)/Vec_Res[i].pageRank))*100;          //calcule ecart_max[i]
@@ -89,45 +89,40 @@ void moy_max(Page Vec_Ex[], Page Vec_Res[], int n){                             
         if (max<ecart_max[i])                                                             
             max = ecart_max[i];
 }
-  moy=moy/(n+1);                                                                                            //divise la somme des ecart_max par le nombre de valeurs sommé, c'est à dire n+1
+  moy=moy/(n+1);                                                                                            //divise la somme des ecart_max par le nombre de valeurs sommees, c'est a dire n+1
   printf("L'écart moyen entre le vecteur éxperimentale et le vecteur résultat est de %.10f %%\n", moy);
   printf("L'écart maximum entre le vecteur éxperimentale et le vecteur résultat est de %.10f %%\n\n", max);
 }
 
 
-void experience(char* filename, int k){
+void algorithm_mat(char* filename, int k){
   FILE* f = fopen(filename,"r");
   if(f == NULL) {
     printf("File not found");
     return;
   }
-  int n = size_matrix(f);
-  List Adj[n+1];
-  rewind(f);                                                                                                 //Recommence à lire le fichier du départ
+  int n = size_matrix(f);                                                                                    //recuperation de la taille de la matrice
+  List Adj[n+1];                                                                                             //declaration du graphe Adj 
+  rewind(f);                                                                                                 //recommence à lire le fichier en partant du debut
   List Adj_pred[n+1];
-  graph_init_mat(f,Adj,Adj_pred, n);
+  graph_init_mat(f,Adj,Adj_pred, n);                                                                         //lecture du fichier tout en remplissant Adj et Adj_pred
   //print_graph(Adj,n);
   //print_graph(Adj_pred,n);
-  Page* Vec_Res = recup_vec(f,n);
-  fclose(f);
+  Page* Vec_Res = recup_vec(f,n);                                                                            //recuperation du vecteur resultat dans le fichier 
+  fclose(f); 
 
-  Page* Vec_Ex = start(Adj, n);
+  Page* Vec_Ex = start(Adj, n);                                                                              //declaration et initialisation du vecteur experimental, résultat de notre algorithme.
   float E;
-  E = (float)1/(10*n);
+  E = (float)1/(10*n);                                                                                       //parametre E fixe
 
-  while (k--)
-  update(Vec_Ex, Adj, Adj_pred, n, E);
-  //print_graph(Adj,n);
-
-  //printf("%s",Vec_Ex[1].name);
-  //free_list(&Adj[0]);
-  //list_free(Vec_Ex);
-  //page_free(Vec_Ex);
+  while (k--)                                                                                                
+  update(Vec_Ex, Adj, Adj_pred, n, E);                                                                       //mise a jour repetee k fois
+  //print_graph(Adj,n);                                                                                      //affichage optionnel du graphe 
   //printf("Vecteur experimental:\n");
-  affiche_res(Vec_Ex,n);
-  moy_max(Vec_Ex, Vec_Res, n);
+  affiche_res(Vec_Ex,n);                                                                                     //affichage optionnel du vecteur experiemental
+  moy_max(Vec_Ex, Vec_Res, n);                                                                               //affichage de l'ecart moyen et de l'ecart maximal
   
-  free_graph(Adj,n);
+  free_graph(Adj,n);                   
   free_graph(Adj_pred);
   free_vec(Vec_Ex, n);
   free_vec(Vec_Res, n);
