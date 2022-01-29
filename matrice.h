41256cac065129c0eedb21d96956a62db3500eb6
salmalys <98278422+salmalys@users.noreply.github.com>
2022-01-29 15:00:27 +0100

    Update matrice.h

--- matrice.h
+++ matrice.h
@@ -1,6 +1,12 @@
+/*Contenu de la bibilotheque matrice
+-fonctions de lecture du fichier et de recuperation des donnees (graphe et vecteur resultat)
+-fonction de comparaison des vecteurs resultat et experimental
+-fonction principale qui declare et initialise les objets Adj, Adj_pred, Vec_Ex et Vec_Res 
+                        effectue la mises à jour k fois 
+                        affiche les resultats*/
+
 int size_matrix(FILE* f);
 void graph_init_mat(FILE* f, List Adj[], List Adj_pred[], int n);
 Page* recup_vec(FILE* f, int n);
-float compare(Page Vec_Ex[], Page Vec_Res[], int n);
 void moy_max(Page Vec_Ex[], Page Vec_Res[], int n);
 void algorithm_mat(char* filename, int k);
