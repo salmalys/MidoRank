f893a98f96b4c2507674971e4ac312f8d492892f
salmalys <98278422+salmalys@users.noreply.github.com>
2022-01-29 15:01:04 +0100

    Update matrice.h

--- matrice.h
+++ matrice.h
@@ -2,8 +2,8 @@
 -fonctions de lecture du fichier et de recuperation des donnees (graphe et vecteur resultat)
 -fonction de comparaison des vecteurs resultat et experimental
 -fonction principale qui declare et initialise les objets Adj, Adj_pred, Vec_Ex et Vec_Res 
-                        effectue la mise à jour k fois 
-                        affiche les resultats*/
+                         effectue la mise à jour k fois 
+                         affiche les resultats*/
 
 int size_matrix(FILE* f);
 void graph_init_mat(FILE* f, List Adj[], List Adj_pred[], int n);
