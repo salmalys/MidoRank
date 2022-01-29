9855f01558abc56502760f0f0d9763ee929bb203
salmalys <98278422+salmalys@users.noreply.github.com>
2022-01-29 14:45:17 +0100

    Update matrice.h

--- matrice.h
+++ matrice.h
@@ -3,4 +3,4 @@ void graph_init_mat(FILE* f, List Adj[], List Adj_pred[], int n);
 Page* recup_vec(FILE* f, int n);
 float compare(Page Vec_Ex[], Page Vec_Res[], int n);
 void moy_max(Page Vec_Ex[], Page Vec_Res[], int n);
-void experience(char* filename, int k);
+void algorithm_mat(char* filename, int k);
