906315b595ee6a1a30d88b89349c1a61655df2ab
salmalys <98278422+salmalys@users.noreply.github.com>
2022-01-30 06:02:00 -0500

    Update wiki.h

--- wiki.h
+++ wiki.h
@@ -2,7 +2,9 @@
 -fonctions de lecture du fichier number_lines, read_link et feed_list
 -foncions de test pour les liens rouges et les doublons
 -fonction d'affichage
--fonction principale algorithm_wiki*/
+-fonction principale qui initialise le graphe
+                         effectue la mise a jour
+                         affiche le podium*/
 
 int number_lines(FILE* f);
 void read_link(List Adj[], FILE* f);
@@ -11,5 +13,5 @@ int test_red_link(char* name, List Adj[], int n);
 int test(char* name, List *l, List Adj[], int n);
 void feed_list(char* line, List* list, List Adj[], int n);
 void graph_init_wiki(FILE* f, List Adj[], int n);
-void affiche_podium(int x, Page* Vec, int n);
+void print_podium(int x, Page* Vec, int n);
 void algorithm_wiki(FILE* file, int x, List Adj[], int n);
